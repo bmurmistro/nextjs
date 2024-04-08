e66eb8567df28eed0670442531ce7b260ee65e01
bmurmistro <bmurmistro@gmail.com>
2024-04-08 17:28:36 -0600

    run cypress

--- cypress/e2e/nextjs.cy.ts
+++ cypress/e2e/nextjs.cy.ts
@@ -1,7 +1,7 @@
 describe('Navigation', () => {
   it('should navigate to the about page', () => {
     // Start from the index page
-    cy.visit('http://localhost:3000/')
+    cy.visit('https://' + Cypress.env('process.env.VERCEL_URL'))
 
     // Find a link with an href attribute containing "about" and click it
     cy.contains('Templates').click()
