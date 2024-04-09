e2d4a48dac0ace5db6600e5538b7ce3c94ea3aea
bmurmistro <bmurmistro@gmail.com>
2024-04-08 19:18:16 -0600

    fix visit

--- cypress/e2e/nextjs.cy.ts
+++ cypress/e2e/nextjs.cy.ts
@@ -1,7 +1,7 @@
 describe('Navigation', () => {
   it('should navigate to the about page', () => {
     // Start from the index page
-    cy.visit()
+    cy.visit('')
 
     // Find a link with an href attribute containing "about" and click it
     cy.contains('Templates').click()
