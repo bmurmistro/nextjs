a1b5d9edb9ea737afc0c60e13736a869b587ae58
bmurmistro <bmurmistro@gmail.com>
2024-04-10 21:05:21 -0600

    Applitools Github Integration

--- cypress/e2e/nextjs.cy.ts
+++ cypress/e2e/nextjs.cy.ts
@@ -1,7 +1,18 @@
 describe('Navigation', () => {
+  beforeEach(function() {
+    cy.eyesOpen({
+      appName: 'nextjs',
+      testName: this.currentTest.title,
+    })
+  })
+  afterEach(() => {
+    cy.eyesClose()
+  })
+
   it('should navigate to the nextjs page', () => {
     // Start from the index page
     cy.visit('/')
+    cy.eyesCheckWindow('Login Page')
 
     // Find a link with an href attribute containing "about" and click it
     cy.contains('Templates').click()
