a4e0722d8c6cb14cade928382c0203385910f393
bmurmistro <bmurmistro@gmail.com>
2024-04-10 21:08:47 -0600

    Remove tag

--- cypress/e2e/nextjs.cy.ts
+++ cypress/e2e/nextjs.cy.ts
@@ -12,9 +12,8 @@ describe('Navigation', () => {
   it('should navigate to the nextjs page', () => {
     // Start from the index page
     cy.visit('/')
-    cy.eyesCheckWindow('Login Page')
+    cy.eyesCheckWindow()
 
-    // Find a link with an href attribute containing "about" and click it
     cy.contains('Templates').click()
   })
 })
